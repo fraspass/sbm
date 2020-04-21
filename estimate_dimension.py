d3d007924b70e1186a0a6c34926bcac04c7f9dd6
fraspass <fraspass@outlook.com>
2020-04-21 14:55:32 +0100

    changes to ZG method

--- estimate_dimension.py
+++ estimate_dimension.py
@@ -8,7 +8,7 @@ from scipy.stats import norm
 ########################################################################
 
 ### Function to estimate the dimension using the automatic method of Zhu and Ghodsi (2006)
-def estimate_dimension(vals,gaps=1):
+def zg_method(vals):
 	## Sort vals if it is not sorted
 	vals = np.sort(vals)[::-1]
 	## Number of possible dimensions
@@ -17,9 +17,19 @@ def estimate_dimension(vals,gaps=1):
 	profile_lik = []
 	## Loop for all the possible values of d
 	for d in range(1,m+1):
-		mu1 = np.mean(vals[:d]) 
+		mu1 = np.mean(vals[:d])
 		mu2 = np.mean(vals[d:])
-		sigma = np.sqrt(np.sum((vals[:d] - mu1) ** 2) + np.sum((vals[d:] - mu2) ** 2) / (m-1))
-		profile_lik += [np.sum(norm.pdf(vals[:d], loc=mu1, scale=sigma)) + np.sum(norm.pdf(vals[d:], loc=mu2, scale=sigma))]
+		sigma = np.sqrt((np.sum((vals[:d] - mu1) ** 2) + np.sum((vals[d:] - mu2) ** 2)) / (m-2))
+		profile_lik += [np.sum(norm.logpdf(vals[:d], loc=mu1, scale=sigma)) + np.sum(norm.logpdf(vals[d:], loc=mu2, scale=sigma))]
 	## Obtain the gaps
-	return np.argsort(-np.array(profile_lik))[:gaps]+1
+	return profile_lik
+
+## Sequential ZG method
+def sequential_zg(vals, k=3, pythonic=True):
+	## Calculate the first dimension using the ZG method
+	d = [np.argsort(zg_method(vals))[-1] + 1]
+	## Repeat for the remaining values 
+	for _ in range(k-1):
+		d += [d[-1] + np.argsort(zg_method(np.sort(vals)[::-1][d[-1]:]))[-1] + 1]
+	## Return list of values of the dimension
+	return np.array(d)-(1 if pythonic else 0)
